604f6c6408c303da70dab132a92d4bdb36736b82
wjh <wjh@trx.com>
2022-11-04 19:24:24 +0800

    first commit

---
+++ src/main/java/com/wjh/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wjh.git;
+
+/**
+ * @author : wjh
+ * @create : 2022-11-04 19:20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
